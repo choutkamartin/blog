7ba205e52f411b50cb76dd38003c8c1072904637
Martin Choutka <martafiixek@gmail.com>
2021-04-11 18:56:18 +0200

    Add localePath with path.resolve

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
+const path = require('path')
+
 module.exports = {
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "cs"],
-  },
-};
+  defaultLocale: 'en',
+  locales: ['en', 'cs'],
+  localePath: path.resolve('./public/static/locales')
+}
